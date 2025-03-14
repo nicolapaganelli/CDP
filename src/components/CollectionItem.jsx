927a1765e46860f0146a47c58b2e114b745c5409
Nicola Paganelli <paganelli.nicola@gmail.com>
2025-03-14 09:44:39 +0100

    Add: CollectionItem component and styles

--- src/components/CollectionItem.jsx
+++ src/components/CollectionItem.jsx
@@ -1,25 +1,17 @@
-import { useState } from 'react';
+import React from 'react';
 
 const CollectionItem = ({ image, title, description, price }) => {
-  const [isHovered, setIsHovered] = useState(false);
-
   return (
-    <div 
-      className="collection-item"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
+    <div className="collection-item">
       <div className="collection-item-image">
         <img src={image} alt={title} />
-        <div className={`collection-item-overlay ${isHovered ? 'active' : ''}`}>
-          <div className="collection-item-details">
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <span className="price">{price}</span>
-            <a href="https://displate.com/limited-edition/displate/8023130" className="cta-button">
-              Purchase Now
-            </a>
-          </div>
+      </div>
+      <div className="collection-item-content">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <div className="collection-item-price">
+          <span>{price}</span>
+          <a href="https://displate.com/limited-edition/displate/8023130" className="cta-button">Buy Now</a>
         </div>
       </div>
     </div>
